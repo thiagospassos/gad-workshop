785fc9dcdae3d8b9d7b37e4fef415bd382db5d5c
Tyson Brown <Tyson@gadsolutions.com.au>
2020-12-09 09:46:02 +1000

    buyaaaar

--- tyson/hello-world/main.js
+++ tyson/hello-world/main.js
@@ -1,6 +1,13 @@
 const app = new Vue({
     el: '#myvue',
     data: {
-        message: 'Tyson\'s TODO List'
+        message: 'Tyson\'s TODO List',
+        todos: [
+            {id: 1, title:"have breakfast",done:false,created: new Date(2010,1,1)},
+            {id: 2, title:"go to work",done:false,created: new Date(2010,1,1)},
+            {id: 3, title:"get lots of money",done:true,created: new Date(2010,1,1)},
+            {id: 4, title:"kill all humans",done:false,created: new Date(2010,1,1)},
+            {id: 5, title:"save the planet",done:false,created: new Date(2010,1,1)},
+        ]
     }
 })
