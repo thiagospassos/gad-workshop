63700ce07810320fc43760409716161d8fc69f4c
Thiago Passos <thiago@passos.com.au>
2020-12-14 14:19:03 +1000

    adding e2e tests

--- northwind-traders/src/views/Home.vue
+++ northwind-traders/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <Counter msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Counter from "@/components/Counter.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    Counter
+    HelloWorld
   }
 };
 </script>
