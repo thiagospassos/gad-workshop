c81d940366091b3c42ce84890f5fab00796b87e8
Thiago Passos <thiago@passos.com.au>
2020-12-14 13:35:51 +1000

    prettifying

--- northwind-traders/tests/unit/invalidfeedback.spec.js
+++ northwind-traders/tests/unit/invalidfeedback.spec.js
@@ -2,26 +2,26 @@ import { shallowMount } from "@vue/test-utils";
 import InvalidFeedback from "@/components/InvalidFeedback.vue";
 
 describe("InvalidFeedback.vue", () => {
-    it("Failed isUnique shows message", () => {
-        const wrapper = shallowMount(InvalidFeedback, {
-            propsData: {
-                model: {
-                    isUnique: false
-                }
-            }
-        });
-        expect(wrapper.text()).toMatch('already exists');
+  it("Failed isUnique shows message", () => {
+    const wrapper = shallowMount(InvalidFeedback, {
+      propsData: {
+        model: {
+          isUnique: false
+        }
+      }
     });
-    it('Failed MinValue shows message', () => {
-        const wrapper = shallowMount(InvalidFeedback, {
-            propsData: {
-                field: 'Name',
-                model: {
-                    minValue: false,
-                    $params: { minValue: { min: 10 } }
-                }
-            }
-        })
-        expect(wrapper.text()).toMatch('Name must be equal or greater than 10.')
-    })
+    expect(wrapper.text()).toMatch("already exists");
+  });
+  it("Failed MinValue shows message", () => {
+    const wrapper = shallowMount(InvalidFeedback, {
+      propsData: {
+        field: "Name",
+        model: {
+          minValue: false,
+          $params: { minValue: { min: 10 } }
+        }
+      }
+    });
+    expect(wrapper.text()).toMatch("Name must be equal or greater than 10.");
+  });
 });
